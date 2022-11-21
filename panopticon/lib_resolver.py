ff2f82c419b68cfb5ca1650b6dbabc8a30bd68ac
aarnav <aarnavbos@gmail.com>
2022-11-21 15:44:51 +0100

    improve get_lib_dir exception message and loop variable

--- panopticon/lib_resolver.py
+++ panopticon/lib_resolver.py
@@ -5,9 +5,9 @@ def get_lib_dir():
     major = sys.version_info.major
     minor = sys.version_info.minor
     if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
-        for i in sys.path:
-            if i.endswith(f'/lib/python{major}.{minor}'):
-                return i
+        for path in sys.path:
+            if path.endswith(f'/lib/python{major}.{minor}'):
+                return path
     raise Exception(
-        "Panopticon cannot automatically resolve Python's lib directory. Please provide it yourself."
+        "Panopticon cannot automatically resolve Python's lib directory. Please provide it yourself with --lib-dir."
     )
